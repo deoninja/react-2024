b592278244489d88c843fd800b9b05c9641593cd
deo <deoninja@gmail.com>
2024-05-07 21:48:50 +0800

    Conditional Rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,36 @@
-import { UserDetails } from './components/UserDetails';
-
 export default function App() {
-  const mockUsers = [
-    {
-      id: 1,
-      username: 'User 1',
-      email: '5uJt9@example.com',
-    },
-    {
-      id: 2,
-      username: 'User 2',
-      email: '5uJt91@example',
-    },
-  ];
+  // const isAuthenticated = true;
+  // return isAuthenticated ? (
+  //   <div>
+  //     <h1>Welcome, User!</h1>
+  //   </div>
+  // ) : (
+  //   <div>
+  //     <h1>You are not logged in</h1>
+  //   </div>
+  // );
 
-  return (
-    <div>
-      {mockUsers.map((user) => {
-        return <UserDetails key={user.id} user={user} />;
-      })}
-    </div>
-  );
+  // NOT_VERIFIED | VERIFIED | ACCOUNT_DISABLED
+  const USER_STATUS = 'NOT_VERIFIED';
+
+  switch (USER_STATUS) {
+    case 'NOT_VERIFIED':
+      return <div>NOT_VERIFIED</div>;
+    case 'VERIFIED':
+      return <div>VERIFIED</div>;
+    case 'ACCOUNT_DISABLED':
+      return <div>ACCOUNT_DISABLED</div>;
+    default:
+      return <div>UNDEFINED</div>;
+  }
+
+  // if (USER_STATUS === 'NOT_VERIFIED') {
+  //   return <div>NOT_VERIFIED</div>;
+  // } else if (USER_STATUS === 'VERIFIED') {
+  //   return <div>VERIFIED</div>;
+  // } else if (USER_STATUS === 'ACCOUNT_DISABLED') {
+  //   return <div>ACCOUNT_DISABLED</div>;
+  // } else {
+  //   return <div>UNDEFINED</div>;
+  // }
 }
