05faf907312b011addafd63f1734e74f79317bfe
deo <deoninja@gmail.com>
2024-05-08 22:08:24 +0800

    States-Array-Update-Element

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
-import { RegisterForm } from './components/RegisterForm';
+import { useState } from 'react';
+import { UserDetails } from './components/UserDetails';
 export default function App() {
-  window.addEventListener('resize', (e) => {
-    console.log(window.innerHeight, window.innerWidth);
-  });
-
-  return <RegisterForm />;
+  const [users, steUsers] = useState([
+    {
+      id: 1,
+      username: 'test',
+      email: 'test@gmali.com',
+    },
+    {
+      id: 2,
+      username: 'test2',
+      email: 'test2@gmali.com',
+    },
+    {
+      id: 3,
+      username: 'test3',
+      email: 'test3@gmali.com',
+    },
+  ]);
+  return (
+    <div>
+      {users.map((user) => (
+        <UserDetails key={user.id} user={user} setUsers={steUsers} />
+      ))}
+    </div>
+  );
 }
--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,27 +1,75 @@
 import propTypes from 'prop-types';
+import { useState } from 'react';
 
-export function UserDetails({ user }) {
+export function UserDetails({ user, setUsers }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [username, setUsername] = useState(user.username);
+  const [email, setEmail] = useState(user.email);
+
   return (
     <div>
-      <b>
-        ID: <span>{user.id}</span>
-      </b>
-      <br />
-      <b>
-        Username: <span>{user.username}</span>
-      </b>
-      <br />
-      <b>
-        Email: <span>{user.email}</span>
-      </b>
+      <div>
+        <button onClick={() => setIsEditing((currentState) => !currentState)}>
+          Edit
+        </button>
+        <button>Delete</button>
+        {isEditing && (
+          <button
+            onClick={() => {
+              setUsers((currentUsersState) => {
+                return currentUsersState.map((currentUser) =>
+                  currentUser.id === user.id
+                    ? { ...currentUser, username, email }
+                    : currentUser
+                );
+              });
+
+              setIsEditing(false);
+            }}
+          >
+            Save
+          </button>
+        )}
+      </div>
+      <div>
+        <b>ID:</b>
+        <br />
+        <b>Username:</b>
+        {isEditing ? (
+          <input
+            name='username'
+            id='username'
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+        ) : (
+          <span>{user.username}</span>
+        )}
+        <br />
+        <b>Email:</b>
+        {isEditing ? (
+          <input
+            type='email'
+            name='email'
+            id='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        ) : (
+          <span>{user.email}</span>
+        )}
+      </div>
     </div>
   );
 }
 
 UserDetails.propTypes = {
   user: propTypes.shape({
-    id: propTypes.number,
-    username: propTypes.string,
+    id: propTypes.number.isRequired,
+    username: propTypes.string.isRequired,
     email: propTypes.string.isRequired,
-  }),
+  }).isRequired,
+  setUsers: propTypes.func.isRequired,
 };
