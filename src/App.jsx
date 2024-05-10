7bf4985dfca05850c0d4ce0fd167515e9160723c
deo <deoninja@gmail.com>
2024-05-10 22:50:31 +0800

    useEffect-Cleanup-function-Abort

--- src/App.jsx
+++ src/App.jsx
@@ -25,12 +25,14 @@ export default function App() {
   // });
 
   useEffect(() => {
+    const controller = new AbortController();
     async function fetchUsers() {
       try {
         const response = await fetch(
           'https://jsonplaceholder.typicode.com/users',
           {
             method: 'GET',
+            signal: controller.signal,
           }
         );
         const json = await response.json();
@@ -40,6 +42,10 @@ export default function App() {
       }
     }
     fetchUsers();
+
+    return () => {
+      controller.abort();
+    };
   });
 
   return (
