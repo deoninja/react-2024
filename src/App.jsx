27fa6525232288e8e6b946f82fda043efebcea7b
deo <deoninja@gmail.com>
2024-05-10 22:42:11 +0800

    Fetching Data Example

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,40 @@ export default function App() {
     document.title = 'React Tutorials ' + counter;
   }, [sync]);
 
+  // useEffect(() => {
+  //   fetch('https://jsonplaceholder.typicode.com/users', {
+  //     method: 'GET',
+  //   }).then((response) => {
+  //     return response
+  //       .json()
+  //       .then((data) => {
+  //         console.log(data);
+  //       })
+  //       .catch((err) => {
+  //         console.log(err);
+  //         console.log(err.message);
+  //       });
+  //   });
+  // });
+
+  useEffect(() => {
+    async function fetchUsers() {
+      try {
+        const response = await fetch(
+          'https://jsonplaceholder.typicode.com/users',
+          {
+            method: 'GET',
+          }
+        );
+        const json = await response.json();
+        console.log(json);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchUsers();
+  });
+
   return (
     <div>
       You click the button {counter} times <br />
