3c16fe7791bd9d95eca369ef55be865e6d0a7827
deo <deoninja@gmail.com>
2024-05-14 22:07:34 +0800

    creating custom hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,15 @@
-import { useState, useEffect } from 'react';
-export default function App() {
-  const [blogPostData, setBlogPostData] = useState({
-    title: '',
-    body: '',
-  });
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { LoginForm } from './components/LoginForm';
 
+export default function App() {
+  const [toggle, setToggle] = useState(false);
   return (
     <div>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          if (blogPostData.title && blogPostData.body) {
-            fetch('https://jsonplaceholder.typicode.com/posts', {
-              method: 'POST',
-              body: JSON.stringify({
-                userId: 1,
-                title: blogPostData.title,
-                body: blogPostData.body,
-              }),
-              headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-              },
-            })
-              .then((response) => response.json())
-              .then((data) => console.log(data))
-              .catch((err) => console.log(err));
-          }
-        }}
-      >
-        <div>
-          <label htmlFor='title'>Title </label>
-          <input
-            type='text'
-            id='title'
-            value={blogPostData.title}
-            onChange={(e) => {
-              setBlogPostData((currentBlogPostData) => ({
-                ...currentBlogPostData,
-                title: e.target.value,
-              }));
-            }}
-          />
-        </div>
-        <div>
-          <label htmlFor='body'>body </label>
-          <input
-            type='text'
-            id='body'
-            value={blogPostData.body}
-            onChange={(e) => {
-              setBlogPostData((currentBlogPostData) => ({
-                ...currentBlogPostData,
-                body: e.target.value,
-              }));
-            }}
-          />
-        </div>
-        <button>create Post</button>
-      </form>
+      <button onClick={() => setToggle((currentState) => !currentState)}>
+        Toggle
+      </button>
+      {toggle && <LoginForm />}
     </div>
   );
 }
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,4 +1,22 @@
+import { useEffect } from 'react';
+import { useDocumentClick } from './utils/hooks/useDocumentClick';
 export function LoginForm() {
+  useEffect(() => {
+    const resizeEventHandler = (e) => {
+      console.log('Window/ViewPort Resized!');
+    };
+
+    window.addEventListener('resize', resizeEventHandler);
+
+    return () => {
+      console.log('Unmounting LoginForm');
+      console.log('Removing Event Listener');
+      window.removeEventListener('resize', resizeEventHandler);
+    };
+  }, []);
+
+  useDocumentClick();
+
   return (
     <div>
       <form
