54387382fdd248cfebc8ea5120ffb32ac650397f
deo <deoninja@gmail.com>
2024-05-21 23:36:56 +0800

    setting-up-route

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,26 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './globals.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { UsersPage } from './pages/UsersPage';
+import { BlogPostPage } from './pages/BlogPostPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/users',
+    element: <UsersPage />,
+  },
+  {
+    path: '/blog-post',
+    element: <BlogPostPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ src/pages/BlogPostPage.jsx
@@ -0,0 +1,7 @@
+export function BlogPostPage() {
+  return (
+    <div>
+      <h1>Welcome to BlogPost Page</h1>
+    </div>
+  );
+}
