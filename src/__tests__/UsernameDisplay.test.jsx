54b8f0300dae20a9642ca7f7da90f0a8cd1e617c
deo <deoninja@gmail.com>
2024-06-06 20:43:32 +0800

    findBytext

--- src/__tests__/UsernameDisplay.test.jsx
+++ src/__tests__/UsernameDisplay.test.jsx
@@ -3,29 +3,10 @@ import { render, screen } from '@testing-library/react';
 import { UsernameDisplay } from '../components/UsernameDisplay';
 
 describe('UsernameDisplay', () => {
-  it('should render username', () => {
+  it('should render username', async () => {
     render(<UsernameDisplay username='DeoTheDeveloper' />);
-    expect(screen.queryByText('DeoTheDeveloper')).not.toBeInTheDocument();
-    // expect(screen.queryByText('DeoTheDeveloper')).toBeNull();
-    // let error;
-    // try {
-    //   screen.getByText('DeoTheDeveloper');
-    // } catch (err) {
-    //   error = err;
-    // }
-    // expect(error).toBeDefined();
-    // expect(screen.getByText('DeoTheDeveloper')).toBeChecked();
-    // expect(result.container).toMatchSnapshot();
-    // expect(screen.getByText('Deothedev')).toBeInTheDocument();
-    // const elements = screen.getAllByText('DeoTheDeveloper');
-    // elements.forEach((element) => expect(element).not.toBeInTheDocument());
+    expect(
+      await screen.findByText('DeoTheDeveloper', {}, { timeout: 10000 })
+    ).toBeInTheDocument();
   });
 });
-
-// describe('UsernameDisplay', () => {
-//   it('should render the username', () => {
-//     render(<UsernameDisplay username='John' />);
-//     const usernameDisplay = screen.getByText('John');
-//     expect(usernameDisplay).toBeInTheDocument();
-//   });
-// });
--- src/components/UsernameDisplay.jsx
+++ src/components/UsernameDisplay.jsx
@@ -1,7 +1,18 @@
+import { useState, useEffect } from 'react';
+
 export function UsernameDisplay({ username }) {
+  const [timerPassed, setTimerPassed] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimerPassed(true);
+    }, 1500);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div>
-      <span></span>
+      <span>{timerPassed && username}</span>
     </div>
   );
 }
